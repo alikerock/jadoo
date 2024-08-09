9bca30daa64dc8ae0dd23b94c6959b2070ddea9e
rock's ezweb <alikerock@gmail.com>
2024-08-09 12:00:33 +0900

    testimonial function add

--- js/script.js
+++ js/script.js
@@ -2,30 +2,55 @@ let pagers = document.querySelectorAll('.testimonials .pager a');
 let testimonialsLists = document.querySelectorAll('.testimonials_list li');
 let paginationUp = document.querySelector('.pagination .up');
 let paginationDown = document.querySelector('.pagination .down');
+let currentIdx = 0;
+let testimonialCount = testimonialsLists.length;
 
-/*
-pagers를 클릭하면 할일
-   모든 a에서 active를 제거하고, 클릭한 그 요소에 active 추가
-
-
-for(let i = 0; i < pagers.length; i++){
-  pagers[i].addEventListener('click',()=>{
-    //모든 a에서 active 제거
-    //pagers[i] 클래스명 추가
-  })
-}
-대상.forEach(function(item,idx,all){
-  item.addEventListener('click',()=>{});
-})
-*/
 pagers.forEach((item,idx)=>{
   item.addEventListener('click',(e)=>{
     e.preventDefault();
-    for(let pager of pagers){
-      pager.classList.remove('active');
-    }
-    item.classList.add('active');
-    //모든 testimonialsLists에서 active를 제거하고 클릭한 그요소의 인덱스번호에 해당하는 list에 active 추가
-    
+    showTestimonial(idx);
   });
+});
+function showTestimonial(num){
+  /*
+    if(num === -1){
+      num = testimonialCount -1;
+    } else if (num === 3){
+      num = 0
+    } else{
+      num = num; 
+    }
+ 
+  num = (num === -1) ? testimonialCount -1 : (num === 3) ? 0: num;
+   */
+  num = (num + testimonialCount) % testimonialCount; 
+  /*
+  num = (1+3) % 3 = 1
+  num1 num1
+
+  num = (2+3) % 3 = 2
+  num2 num2
+
+  num = (3+3) % 3 = 0
+  num3 num0
+  */
+
+  for(let testimonial of testimonialsLists){
+    testimonial.classList.remove('active');
+  }
+  testimonialsLists[num].classList.add('active');
+  currentIdx = num;
+
+  for(let pager of pagers){
+    pager.classList.remove('active');
+  }
+  pagers[num].classList.add('active');
+
+}
+
+paginationDown.addEventListener('click',()=>{
+  showTestimonial(currentIdx + 1);
+});
+paginationUp.addEventListener('click',()=>{
+  showTestimonial(currentIdx - 1);
 });
